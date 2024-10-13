9eb9c9e039432b9c314e56b1133767ffc0d16511
Thien Tran <fcduythien@gmail.com>
2024-10-13 10:02:25 +0700

    test

--- src/main.rs
+++ src/main.rs
@@ -29,3 +29,14 @@ async fn main() -> Result<(), Error> {
 
     Ok(())
 }
+
+fn add(x: u32, y: u32) -> u32 {
+    x + y
+}
+mod tests {
+    use super::*;
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+}
